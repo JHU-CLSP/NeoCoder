00a561f92db560f1cf6a4a77bda7a21c05f85362
Yining Lu <kinglu20000216@gmail.com>
2024-07-16 09:59:58 +0800

    Update detect_techniques.sh

--- scripts/detect_techniques.sh
+++ scripts/detect_techniques.sh
@@ -1,4 +1,4 @@
 python steps/creativity_evaluation.py \
     --task detection \
     --inference-result-path datasets/CodeForce/evaluation/anthropic.claude-3-sonnet-20240229-v1:0_sample=199_dp=5_creativity.json \
-    --human-solution-path datasets/CodeForce/DPCode/human_solutions.json
+    --human-solution-path datasets/CodeForce/NeoCoder/human_solutions.json
